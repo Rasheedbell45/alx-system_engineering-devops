a7d75856830d91bc3917867e879547428906f707
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-30 19:51:40 +0100

    Create test_alias.sh

---
+++ 0x03-shell_variables_expansions/test_alias.sh
@@ -0,0 +1,14 @@
+0-alias
+file_1
+file_2
+test_alias.sh
+total 8
+-rw-r--r-- 1 root root 71 Jul 30 11:48 0-alias
+-rw-r--r-- 1 root root  0 Jul 30 11:48 file_1
+-rw-r--r-- 1 root root  0 Jul 30 11:48 file_2
+-rwxr-xr-x 1 root root 62 Jul 30 11:48 test_alias.sh
+
+(236 chars long)
+
+[Expected]
+total 0
